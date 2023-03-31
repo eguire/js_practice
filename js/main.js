d8d55f2daef67f7cdb63da9ddf79ff31cc4695c8
Eduard Honcharov <bad.sand.king@gmail.com>
2023-03-31 22:18:38 +0300

    Decided two tasks

--- js/main.js
+++ js/main.js
@@ -1,19 +1,30 @@
-const numberOfFilms = +prompt('Cколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
-	count: numberOfFilms,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false
-};
 
-const a = prompt('Один из порследних фильмов?', ''),
-			b = prompt('На скок оцените?', ''),
-			c = prompt('Один из порследних фильмов?', ''),
-			d = prompt('На скок оцените?', '');
+function calculateVolumeAndArea(length) {
+	if (Number.isInteger(length) && !(length <= 0) && !isNaN(length)) {
+		return result = `Объем куба: ${Math.pow(length, 3)}, площадь всей поверхности: ${6 * Math.pow(length, 2)}`;
+	} else {
+		return 'При вычислении произошла ошибка';
+	}
+}
+console.log(calculateVolumeAndArea(5));
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+function getCoupeNumber(num) {
+	if (!isNaN(num) && !(num < 0) && Number.isInteger(num)) {
+		if (num !== 0 && !(num > 36)) {
+			for (let i = 1; i <= 9; i++) {
+				if (num <= i * 4) {
+					return i;
+				} else {
+					continue;
+				}
+			}
+		} else {
+			return 'Таких мест в вагоне не существует'
+		}
+	} else {
+		return 'Проверьте правильность введенного номера места'
+	}
+}
 
-console.log(personalMovieDB);
+console.log(getCoupeNumber(6))
