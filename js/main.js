e559afca19b8b0400faa41849d6a937bce005fbb
Eduard Honcharov <bad.sand.king@gmail.com>
2023-04-01 14:55:04 +0300

    Decided numbers Fibbonachi

--- js/main.js
+++ js/main.js
@@ -1,30 +1,26 @@
 
-
-function calculateVolumeAndArea(length) {
-	if (Number.isInteger(length) && !(length <= 0) && !isNaN(length)) {
-		return result = `Объем куба: ${Math.pow(length, 3)}, площадь всей поверхности: ${6 * Math.pow(length, 2)}`;
-	} else {
-		return 'При вычислении произошла ошибка';
-	}
-}
-console.log(calculateVolumeAndArea(5));
-
-function getCoupeNumber(num) {
-	if (!isNaN(num) && !(num < 0) && Number.isInteger(num)) {
-		if (num !== 0 && !(num > 36)) {
-			for (let i = 1; i <= 9; i++) {
-				if (num <= i * 4) {
-					return i;
+function fib(num) {
+	if (Number.isInteger(num) && num > 0 && !isNaN(num)) {
+		let fibarr = [0, 1];
+		let result = '';
+		if (num === 1) {
+			return '0';
+		} else if (num === 2) {
+			return '0 1';
+		} else {
+			for (let i = 2; i < num; i++) {
+				fibarr[i] = fibarr[i - 1] + fibarr[i - 2];
+			}
+			for (let j = 0; j < fibarr.length; j++) {
+				if (j === 0) {
+					result += `${fibarr[j]}`
 				} else {
-					continue;
+					result += ` ${fibarr[j]}`
 				}
 			}
-		} else {
-			return 'Таких мест в вагоне не существует'
 		}
-	} else {
-		return 'Проверьте правильность введенного номера места'
+		return result
 	}
+	return ''
 }
-
-console.log(getCoupeNumber(6))
+console.log(fib(7))
