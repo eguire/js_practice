ee55d94ce50e8e065aaf7cafc61ce1fc8ae7b822
Eduard Honcharov <bad.sand.king@gmail.com>
2023-04-05 12:30:52 +0300

    work with objects

--- js/main.js
+++ js/main.js
@@ -1,26 +1,37 @@
+const personalPlanPeter = {
+	name: "Peter",
+	age: "29",
+	skills: {
+		languages: ['ru', 'eng'],
+		programmingLangs: {
+			js: '20%',
+			php: '10%'
+		},
+		exp: '1 month'
+	},
+	showAgeAndLangs: function (plan) {
+		const { age } = plan;
+		const { languages } = plan.skills;
+		let str = `Мне ${age} и я владею языками: `;
 
-function fib(num) {
-	if (Number.isInteger(num) && num > 0 && !isNaN(num)) {
-		let fibarr = [0, 1];
-		let result = '';
-		if (num === 1) {
-			return '0';
-		} else if (num === 2) {
-			return '0 1';
-		} else {
-			for (let i = 2; i < num; i++) {
-				fibarr[i] = fibarr[i - 1] + fibarr[i - 2];
-			}
-			for (let j = 0; j < fibarr.length; j++) {
-				if (j === 0) {
-					result += `${fibarr[j]}`
-				} else {
-					result += ` ${fibarr[j]}`
-				}
-			}
-		}
-		return result
+		languages.forEach(function (lang) {
+			str += `${lang.toUpperCase()} `;
+		});
+
+		return str;
 	}
-	return ''
-}
-console.log(fib(7))
+};
+
+// function showExperience(plan) {
+// 	let { exp } = plan.skills;
+// 	return `${exp}`;
+// }
+
+// function showProgrammingLangs(plan) {
+// 	let result = '';
+// 	let langsArr = plan.skills.programmingLangs;
+// 	for (let keys in langsArr) {
+// 		result += `Язык ${keys} изучен на ${langsArr[keys]}\n`
+// 	}
+// 	return result;
+// }
